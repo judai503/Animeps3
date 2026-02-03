bcdf7959569033ecf8bf3abd579c3c53787a35aa
El tío judai <alexnarcista@gmail.com>
2026-02-03 10:03:13 -0600

    Refactor service and page creation for AnimePS3

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-var service = require('showtime/service');
+var page = require('movian/page');
+var service = require('movian/service');
 
-service.create(
-  "AnimePS3 TEST",
-  "animeps3:test",
-  "video",
-  true
-);
+service.create("AnimePS3", "animeps3:start", "video", true);
+
+page.Route("animeps3:start", function (page) {
+    page.metadata.title = "AnimePS3";
+    page.appendItem("", "directory", {
+        title: "Funciona correctamente"
+    });
+    page.loading = false;
+});
